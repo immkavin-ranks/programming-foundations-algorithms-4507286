f7b5e0d42ee996bd5cadf210b1788d3b4c5bca9a
Kavin <immkavin.ofcl@gmail.com>
2024-06-07 06:59:42 +0000

    data structures

--- Start/DataStructures/Dictionary/dictionary_start.py
+++ Start/DataStructures/Dictionary/dictionary_start.py
@@ -1,17 +1,29 @@
 # Example file for Programming Foundations: Algorithms by Joe Marini
 # demonstrate dictionary usage
 
+# speed
+# unordered
+
 
 # TODO: create a dictionary all at once
+items1 = dict({"key1": 1, "key2": 2, "key3": 3})
 
-
+print(items1)
 # TODO: create a dictionary progressively
+items2 = {}
+items2["key2"] = 1
+items2["key4"] = 5
+items2["key3"] = 3
+print(items2)
 
 
 # TODO: replace an item
-
+items2["key4"] = 5
+print(items2)
 
 # TODO: try to access a nonexistent key
-
+# print(items1["key"])
 
 # TODO: iterate the keys and values in the dictionary
+for key, item in items2.items():
+    print("key:", key, "value:", item)
--- Start/DataStructures/Queues/queue_start.py
+++ Start/DataStructures/Queues/queue_start.py
@@ -1,10 +1,19 @@
 # Example file for Programming Foundations: Algorithms by Joe Marini
 # try out the Python queue functions
-
+from collections import deque
 
 # TODO: create a new empty deque object that will function as a queue
 
+# list O(n)
 
+queue = deque()
+
+queue.append(1)
+queue.append(3)
+queue.append(4)
+queue.append(15)
+
+print(queue)
 # TODO: add some items to the queue
 
 
@@ -12,3 +21,10 @@
 
 
 # TODO: pop an item off the front of the queue
+x = queue.popleft()
+
+print(x)
+print(queue)
+y = queue.pop()
+print(y)
+print(queue)
